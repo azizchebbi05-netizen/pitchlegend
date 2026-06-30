4f6366fc0d91ac9e351082fb847de476c18d6c0a
Aziz Chebbi <azizchebbi05@gmail.com>
2026-06-30 15:11:58 +0100

    fix Login import

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
+
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Players from "./components/Players";
 import Services from "./components/services";
-import contact from "./components/contact";
+import Contact from "./components/contact";
 import Messages from "./components/Messages";
 import Footer from "./components/Footer";
-import { useCallback, useMemo, useState } from "react";
 import Login from "./components/Login";
+
 export default function App() {
   const [isAdmin, setIsAdmin] = useState(false);
 
@@ -16,9 +18,13 @@ export default function App() {
 
   return (
     <div>
-      <h1>Admin Dashboard</h1>
+      <Header />
+      <Hero />
+      <Players />
+      <Services />
+      <Contact />
       <Messages />
+      <Footer />
     </div>
   );
 }
-export default Login;
